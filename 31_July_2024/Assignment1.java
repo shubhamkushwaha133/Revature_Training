7eb49334c0bf000b74e7d37bc2810f4bd4ec681f
SHUBHAM KUSHWAHA <shubhamkushwaha1304@gmail.com>
2024-08-01 01:08:29 +0530

    adding class assignment on 31 july 2024

---
+++ 31_July_2024/Assignment1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int length=sc.nextInt();
+	    int breadth=sc.nextInt();
+	    if(length==breadth){
+	        System.out.println("Yes, this is a square");
+	    }
+	}
+}
